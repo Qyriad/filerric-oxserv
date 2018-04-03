6fc7c7b7d9ecd44a4a413d8a6c925f2633f632fd
Mikaela RJ Szekely <qyriad@gmail.com>
2018-04-03 14:28:35 -0400

    Project2: Add successful sending of data back and forth

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 #![feature(ip_constructors)]
 
 use std::net::{TcpListener, Ipv4Addr};
+use std::io;
+use std::io::{BufRead, BufReader, Write, BufWriter};
+use std::thread;
+
+extern crate crossbeam;
 
 fn main()
 {
@@ -15,6 +20,44 @@ fn main()
             Ok(client) =>
             {
                 println!("New client peer: {:?}", client.peer_addr());
+                let mut reader = BufReader::new(&client);
+                let mut writer = BufWriter::new(&client);
+                crossbeam::scope(|scope| // Threads spawned in this scope will be destroyed at the end of said scope
+                {
+                    scope.spawn(||
+                    {
+                        println!("Thread for receiving data from client: {:?}", client.peer_addr());
+                        for line in reader.lines()
+                        {
+                            match line
+                            {
+                                Ok(line) =>
+                                {
+                                    println!("\t{}", line);
+                                }
+                                Err(err) => println!("No data: {:?}", err)
+                            }
+                        }
+                        println!("We're out of lines from {:?}", client.peer_addr());
+                    });
+                    scope.spawn(||
+                    {
+                        println!("Thread for sending data to client {:?}", client.peer_addr());
+                        let stdin = BufReader::new(io::stdin());
+                        for line in stdin.lines()
+                        {
+                            match line
+                            {
+                                Ok(line) =>
+                                {
+                                    write!(&mut writer, "{}\n", line);
+                                    writer.flush().unwrap();
+                                }
+                                Err(err) => println!("No data from stdin: {:?}", err)
+                            }
+                        }
+                    });
+                });
             }
             Err(_) =>
             {
